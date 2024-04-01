3f5010c4f67835d3e496f3a7c2bb3708e493a9f3
408b7f8b <davbr@tutanota.com>
2024-04-01 16:27:33 +0200

    masterbranch aktualisiert

--- Program.cs
+++ Program.cs
@@ -11,44 +11,70 @@ namespace isci.zykluszeit
 {
     class Program
     {
+        public class Konfiguration : Parameter
+        {
+            [fromArgs, fromEnv]
+            public uint anlaufZyklen = 100;
+            public Konfiguration(string[] args) : base(args) { }
+        }
+
         static void Main(string[] args)
         {
-            var konfiguration = new Parameter(args);
+            var konfiguration = new Konfiguration(args);
 
             var structure = new Datenstruktur(konfiguration);
+            var ausfuehrungsmodell = new Ausführungsmodell(konfiguration, structure.Zustand);
 
-            var dm = new Datenmodell(konfiguration);
-            var zykluszeit = new dtInt32(0, "LetzteZykluszeit");
+            var dm = new Datenmodell(konfiguration.Identifikation);
+
+            var zykluszeit = new dtDouble(0.0, "zykluszeit");
+            var zykluszeit_max = new dtDouble(0.0, "zykluszeit_max");
             dm.Dateneinträge.Add(zykluszeit);
+            dm.Dateneinträge.Add(zykluszeit_max);
 
-            var beschreibung = new Modul(konfiguration.Identifikation, "isci.zykluszeit", new ListeDateneintraege(){zykluszeit});
-            beschreibung.Name = "Zykluszeit Ressource " + konfiguration.Identifikation;
-            beschreibung.Beschreibung = "Modul zur Zykluszeitermittlung";
-            beschreibung.Speichern(konfiguration.OrdnerBeschreibungen + "/" + konfiguration.Identifikation + ".json");
+            new Modul(konfiguration.Identifikation, "isci.zykluszeit", new ListeDateneintraege() { zykluszeit, zykluszeit_max })
+            {
+                Name = "Zykluszeit Ressource " + konfiguration.Identifikation,
+                Beschreibung = "Modul zur Zykluszeitermittlung"
+            }.Speichern(konfiguration.OrdnerBeschreibungen + "/" + konfiguration.Identifikation + ".json");
 
             dm.Speichern(konfiguration.OrdnerDatenmodelle + "/" + konfiguration.Identifikation + ".json");
 
             structure.DatenmodellEinhängen(dm);
             structure.Start();
 
-            var ausfuehrungsmodell = new Ausführungsmodell(konfiguration, structure.Zustand);
+            zykluszeit_max.Wert = 0.0;
+            zykluszeit_max.WertInSpeicherSchreiben();
 
             long curr_ticks = 0;
-            
-            while(true)
+
+            int i = 0;
+
+            while (true)
             {
                 structure.Zustand.WertAusSpeicherLesen();
 
-                if (ausfuehrungsmodell.AktuellerZustandModulAktivieren()){
+                if (ausfuehrungsmodell.AktuellerZustandModulAktivieren())
+                {
                     var curr_ticks_new = System.DateTime.Now.Ticks;
                     var ticks_span = curr_ticks_new - curr_ticks;
                     curr_ticks = curr_ticks_new;
-                    zykluszeit.Wert = (System.Int32)(ticks_span / System.TimeSpan.TicksPerMillisecond);
+                    zykluszeit.Wert = (double)ticks_span / System.TimeSpan.TicksPerMillisecond;
                     zykluszeit.WertInSpeicherSchreiben();
+
+                    if (zykluszeit.Wert > zykluszeit_max.Wert && i > konfiguration.anlaufZyklen)
+                    {
+                        zykluszeit_max.Wert = zykluszeit.Wert;
+                        zykluszeit_max.WertInSpeicherSchreiben();
+                    }
+
+                    if (i <= konfiguration.anlaufZyklen) ++i;
 
                     ausfuehrungsmodell.Folgezustand();
                     structure.Zustand.WertInSpeicherSchreiben();
                 }
+
+                System.Threading.Thread.Sleep(1);
             }
         }
     }
